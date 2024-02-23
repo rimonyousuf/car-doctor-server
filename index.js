bb5f06fff5fbe187c6f672e1c1407d8a9ac3c546
Rimon <rimonyousuf00@gmail.com>
2024-02-23 06:52:42 +0600

    Book collection patch and delete has been added

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const options = {
-                projection: { title: 1, price: 1, service_id: 1 },
+                projection: { title: 1, price: 1, service_id: 1, img: 1 },
             };
             const result = await serviceCollection.findOne(query, options);
             res.send(result);
@@ -45,8 +45,39 @@ async function run() {
 
         // bookings
 
-        app.post('/bookings',async(req,res)=>{
-            const user = req.body
+        app.post('/bookings', async (req, res) => {
+            const user = req.body;
+            const result = await bookingCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.get('/bookings', async (req, res) => {
+            let query;
+            if(req.query?.email){
+                query = {email: req.query.email}
+            }
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.patch('/bookings/:id',async(req,res)=>{
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const updateBooking = req.body;
+            const updateDoc = {
+                $set: {
+                    status: updateBooking.status
+                },
+            }
+            const result = await bookingCollection.updateOne(filter,updateDoc);
+            res.send(result);
+        })
+
+        app.delete('/bookings/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
