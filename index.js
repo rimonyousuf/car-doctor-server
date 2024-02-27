b954b16d76c8c6da108b7efadc66710493eed85b
Rimon <rimonyousuf00@gmail.com>
2024-02-27 09:33:53 +0600

    Email sort has been added

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h8beuz9.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,6 +25,27 @@ const client = new MongoClient(uri, {
     }
 });
 
+const logger = async (req, res, next) => {
+    console.log('called:', req.host, req.originalUrl);
+    next()
+}
+
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token;
+    console.log("Value of the token is", token);
+    if (!token) {
+        return res.status(401).send({ message: 'Forbidden' });
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+            return res.status(401).send({ message: 'unauthorized' });
+        }
+        console.log('Value in the token', decoded);
+        req.user = decoded;
+        next()
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -26,6 +53,19 @@ async function run() {
 
         const serviceCollection = client.db("carDoctor").collection("services");
         const bookingCollection = client.db("carDoctor").collection("bookings");
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            console.log(user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false,
+                    sameSite: 'none'
+                })
+                .send({ success: true });
+        })
 
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
@@ -51,31 +91,34 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/bookings', async (req, res) => {
-            let query;
-            if(req.query?.email){
-                query = {email: req.query.email}
+        app.get('/bookings', logger, verifyToken, async (req, res) => {
+            if(req.query.email !== req.user.email){
+                return res.status(403).send({message: 'Forbidden access'})
+            }
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
             }
             const result = await bookingCollection.find(query).toArray();
             res.send(result);
         })
 
-        app.patch('/bookings/:id',async(req,res)=>{
+        app.patch('/bookings/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)};
+            const filter = { _id: new ObjectId(id) };
             const updateBooking = req.body;
             const updateDoc = {
                 $set: {
                     status: updateBooking.status
                 },
             }
-            const result = await bookingCollection.updateOne(filter,updateDoc);
+            const result = await bookingCollection.updateOne(filter, updateDoc);
             res.send(result);
         })
 
-        app.delete('/bookings/:id',async(req,res)=>{
+        app.delete('/bookings/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await bookingCollection.deleteOne(query);
             res.send(result);
         })
